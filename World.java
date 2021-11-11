ab84a4cc9974c48ae5cbd710e8d390906a78f966
Anastasia Zanyuk <nzanyuk@gmail.com>
2021-11-11 15:34:01 +0000

    Added the world class

---
+++ World.java
@@ -0,0 +1,15 @@
+import ansi_terminal.*;
+
+public class World{
+  private int roomNum=0;
+
+  //keeps track of room change?
+  //once the player goes to the next room - increment level
+
+  public int getRoomNum(){
+      return roomNUm;
+  }
+   
+
+
+}
