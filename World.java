fc9c8c9fdbe482ee695bf77b01f0dca88ceaa904
Anastasia Zanyuk <nzanyuk@gmail.com>
2021-11-14 19:13:08 +0000

    added scanner things to World

--- World.java
+++ World.java
@@ -2,9 +2,18 @@ import ansi_terminal.*;
 
 public class World{
   private int roomNum=0;
-
+  private ArrayList<String> rooms = new ArrayList<String>();
+  rooms.add(Room1);
+  rooms.add(Room2);
+  rooms.add(Room3);
   //keeps track of room change?
   //once the player goes to the next room - increment level
+  for(String filename: rooms){
+    World(String filename) throws exception{
+      Scanner s = new Scanner(new FileReader(filename));
+    //...
+    }
+  }  
 
   public int getRoomNum(){
       return roomNum;
