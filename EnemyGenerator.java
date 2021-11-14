6ef0998876694da5c6afbecf80c98f8ab8d3d490
Tracy Beer <tracybeer35@gmail.com>
2021-11-14 19:38:00 +0000

    Added Room3, finsihed Room2, and added 2 new enemies

--- EnemyGenerator.java
+++ EnemyGenerator.java
@@ -1,12 +1,20 @@
 // EnemyGenerator.java
 // this class contains a static method for creating enemies randomly
-
+import java.util.ArrayList;
 import java.util.Random;
 
 public class EnemyGenerator {
+
+    private static Random rng = new Random(); 
+
     public static Enemy generate(int row, int col) {
-        // TODO: replace this with your own code!
-        return new Enemy("Alien", row, col, 20, 5, 3);
+	ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+        enemies.add(Enemy("Alien", row, col, 20, 5, 3));
+	enemies.add(Enemy("Big Alien", row, col, 50, 10, 6));
+	enemies.add(Enemy("Really Big Alien", row, col, 80, 20, 10);
+
+	int n = rng.nextInt(3);
+	return enemies.get(n);
     }
 }
 
